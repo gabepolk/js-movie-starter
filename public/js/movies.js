637163576b465cc3a949908abc2344039506c4e8
Gabe Polk <gabe.a.polk@gmail.com>
2014-07-07 14:03:13 -0700

    Added movie details page and made everything AJAXy

--- public/js/movies.js
+++ public/js/movies.js
@@ -4,24 +4,31 @@
     $.get("https://api.themoviedb.org/3/movie/now_playing", { api_key: "85890c052e1f59ee8257fc442dd6066f" }, function(data){
       var source = $("#movie-release-template").html();
       var template = Handlebars.compile(source);
+      var movieArr = data.results
 
-      for (var i = 0; i < data.results.length; i++) {
-        html = template(data.results[i]);
+      for (var i = 0; i < movieArr.length; i++) {
+        html = $(template(movieArr[i]));
+        html.click(function() {
+          showMovie($(this).attr("data-id"));
+        });
         $('.main').prepend(html);
       };
-
-      // $.each(data.results, function( index, value ) {
-      //   $(".main").append("<div>" +
-      //                       "<h2>" + value.title + "</h2>" +
-      //                       "<img src='http://image.tmdb.org/t/p/w185/" + value.poster_path + "' />" +
-      //                       "<p>Rating: " + value.vote_average + "</p>" +
-      //                     "</div>");
-      //   // console.log(value.title);
-      // });
     });
 
-
-
+    var showMovie = function(id) {
+      $.ajax({
+        type: "GET",
+        url: "https://api.themoviedb.org/3/movie/" + id,
+        data: { api_key: "85890c052e1f59ee8257fc442dd6066f" },
+        dataType: 'json',
+        success: function(movie){
+          var source = $("#movie-detail-template").html();
+          var template = Handlebars.compile(source);
+          html = template(movie);
+          $('.main').html(html);
+        }
+      });
+    };
 
   });
 })()
