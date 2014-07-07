f83376d159dafe3ed42f7db91a67ad6070f7cd13
Gabe Polk <gabe.a.polk@gmail.com>
2014-07-07 15:23:31 -0700

    Added search via the movie db API

--- public/js/movies.js
+++ public/js/movies.js
@@ -11,7 +11,7 @@
         html.click(function() {
           showMovie($(this).attr("data-id"));
         });
-        $('.main').prepend(html);
+        $('.main-movies').prepend(html);
       };
     });
 
@@ -25,10 +25,34 @@
           var source = $("#movie-detail-template").html();
           var template = Handlebars.compile(source);
           html = template(movie);
-          $('.main').html(html);
+          $('.main-movies').html(html);
         }
       });
     };
+
+    $("#enter-search").click(function(){
+      var query = $(".search").val();
+      $.ajax({
+        type: "GET",
+        url: "https://api.themoviedb.org/3/search/movie",
+        data: { api_key: "85890c052e1f59ee8257fc442dd6066f", query: query },
+        dataType: 'json',
+        success: function(data){
+          var source = $("#search-results-template").html();
+          var template = Handlebars.compile(source);
+          var searchArr = data.results
+
+          console.log(searchArr);
+
+          $('.main-movies').empty();
+          for (var i = 0; i < searchArr.length; i++) {
+            html = $(template(searchArr[i]));
+            $('.main-movies').append(html)
+          };
+        }
+      });
+
+    })
 
   });
 })()
